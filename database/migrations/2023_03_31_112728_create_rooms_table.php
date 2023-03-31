5997060ee229c24c033757de5d33f63a42e51d05
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-03-31 11:59:04 +0000

    Create rooms migration

--- database/migrations/2023_03_31_112728_create_rooms_table.php
+++ database/migrations/2023_03_31_112728_create_rooms_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -17,6 +16,7 @@ return new class extends Migration
             $table->unsignedBigInteger('roomType_id');
             $table->foreign('roomType_id')->references('id')->on('room_types')->cascadeOnDelete();
             $table->timestamps();
+
         });
     }
 
