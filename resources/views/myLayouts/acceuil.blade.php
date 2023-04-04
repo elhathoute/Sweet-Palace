2892ea810a6466f5712e4f74358080c8563a5c52
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-04 01:04:26 +0000

    create welcome page

---
+++ resources/views/myLayouts/acceuil.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- links bootstrap  start--}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    {{-- links bootstrap  end--}}
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    {{-- CSS Links--}}
+    <link href="{{asset('/css/acceuil.css')}}" rel="stylesheet">
+    <title>Sweet Palace</title>
+</head>
+<body>
+    <div>
+        @extends('myLayouts.footer')
+    </div>
+    <div>
+        @extends('myLayouts.navbar')
+    </div>
+
+</body>
+{{-- links bootstrap  start--}}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+{{-- links bootstrap  end--}}
+</html>
