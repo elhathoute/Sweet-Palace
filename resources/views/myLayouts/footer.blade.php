16bf95be383dc3b23f2673f4565e67c1602f119e
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-04 01:04:05 +0000

    Footer

---
+++ resources/views/myLayouts/footer.blade.php
@@ -0,0 +1,30 @@
+<footer style="background-color:#393E46;">
+    <div class="container mt-5 py-5">
+
+        <div class="d-flex justify-content-around mt-5">
+            <div class="d-flex flex-column text-white me-4">
+                <p>Sweet Palace</p>
+                <p class="">We are committed to meeting all of your</p>
+                <p class="">expectations and making your stay </p>
+                <p class="mb-5">as comfortable as possible.</p>
+            </div>
+            <div class="d-flex flex-column text-white mt-5">
+                <p class=""><i class='bx bx-map fs-5 mx-1'></i>Boulevard moulay youssef,</p>
+                <p class="ms-4"> Marrakech, Maroc</p>
+            </div>
+        </div>
+        <hr class="pb-5 text-white text-center">
+        <div class="d-flex align-items-center justify-content-between">
+            <div class="text-center py-2">
+                <p class="text-white">© 2023 SWEET PALACE- All rights reserved</p>
+            </div>
+            <div class="d-flex flex-nowrap justify-content-center">
+                <a href="#" style="color: white;"><i id="bx1" class='bx bxl-facebook fs-1 mx-2'></i></a>
+                <a href="#" style="color: white;"><i id="bx2" class='bx bxl-twitter fs-1 mx-2' ></i></a>
+                <a href="#" style="color: white;"><i id="bx3" class='bx bxl-instagram fs-1 mx-2' ></i></a>
+                <a href="#" style="color: white;"><i id="bx4" class='bx bxl-youtube fs-1 mx-2' ></i></a>
+                <a href="#" style="color: white;"><i id="bx5" class='bx bxl-whatsapp fs-1 mx-2' ></i></a>
+            </div>
+        </div>
+    </div>
+</footer>
