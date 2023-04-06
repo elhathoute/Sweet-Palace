2819e304a3cc54d66e63fc4dd08e0e3d4822dce9
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-06 15:56:38 +0000

    add room section

---
+++ resources/views/myLayouts/rooms.blade.php
@@ -0,0 +1,58 @@
+<section id="rooms" class="rooms_wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-12 text-center mb-5">
+                <h6>What Can I do For you</h6>
+                <h3 class="section-title">Our Favorite Rooms</h3>
+                <hr class="line">
+            </div>
+        </div>
+        <div class="row m-0">
+            <div class="col-md-4 mb-4 mb-lg-0">
+                <div class="room-items">
+                    <img src="{{asset('/images/royalSuit.jpg')}}" class="room img-fluid" alt="">
+                </div>
+                <div class="room-item-wrap">
+                    <div class="room-content">
+                        <h5 class="text-black mb-lg-3 text-decoration-underline">Royal Suit</h5>
+                        <p class="text-black"> Discover Five of our Favorite dresses </p>
+                        <p class="text-black">from our new collection that destined</p>
+                        <p class="text-black">to be worn and loved Immediately.</p>
+                        <p class="text-black fw-bold my-lg-4"> 220 euro / per night</p>
+                        <a class="main-btn text-decoration-none btn-outline-dark rounded-2 px-lg-4 py-lg-2" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4 mb-lg-0">
+                <div class="room-items">
+                    <img src="{{asset('/images/double.jpg')}}" class="room img-fluid" alt="">
+                </div>
+                <div class="room-item-wrap">
+                    <div class="room-content">
+                        <h5 class="text-black mb-lg-3 text-decoration-underline">Double Room</h5>
+                        <p class="text-black"> Discover Five of our Favorite dresses </p>
+                        <p class="text-black">from our new collection that destined</p>
+                        <p class="text-black">to be worn and loved Immediately.</p>
+                        <p class="text-black fw-bold my-lg-4"> 220 euro / per night</p>
+                        <a class="main-btn text-decoration-none btn-outline-dark rounded-2 px-lg-4 py-lg-2" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4 mb-4 mb-lg-0">
+                <div class="room-items">
+                    <img src="{{asset('/images/single.jpg')}}" class="room img-fluid" alt="">
+                </div>
+                <div class="room-item-wrap">
+                    <div class="room-content">
+                        <h5 class="text-black mb-lg-3 text-decoration-underline">Single Room</h5>
+                        <p class="text-black"> Discover Five of our Favorite dresses </p>
+                        <p class="text-black">from our new collection that destined</p>
+                        <p class="text-black">to be worn and loved Immediately.</p>
+                        <p class="text-black fw-bold my-lg-4"> 220 euro / per night</p>
+                        <a class="main-btn text-decoration-none btn-outline-dark rounded-2 px-lg-4 py-lg-2" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
