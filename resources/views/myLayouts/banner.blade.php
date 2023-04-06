2869f456d31f02890ad5adbbec195459ec59465f
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-06 06:31:10 +0000

    add banner

---
+++ resources/views/myLayouts/banner.blade.php
@@ -0,0 +1,82 @@
+<section id="home" class="banner_wrapper p-0">
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide" style="background-image: url('../../images/hotel1.jpg')">
+                <div class="slide-caption text-center">
+                    <div>
+                        <p class="slide-title">Welcome to our hotel</p>
+                        <p class="slide-text">We are delighted to have you as our guest and to offer you an unforgettable experience. Our hotel features comfortable rooms, quality service, and modern facilities to meet all your needs. </p>
+                        <p class="slide-text">We hope you will have a pleasant and memorable stay with us.</p>
+                    </div>
+                 </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('../../images/hotel2.jpg')">
+                <div class="slide-caption text-center">
+                    <div>
+                        <p class="slide-title">Welcome to our hotel </p>
+                        <p class="slide-text">We are delighted to have you as our guest and to offer you an unforgettable experience. Our hotel features comfortable rooms, quality service, and modern facilities to meet all your needs. </p>
+                        <p class="slide-text">We hope you will have a pleasant and memorable stay with us.</p>
+                    </div>
+                 </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('../../images/hotel3.jpg')">
+                <div class="slide-caption text-center">
+                    <div>
+                        <p class="slide-title">Welcome to our hotel </p>
+                        <p class="slide-text">We are delighted to have you as our guest and to offer you an unforgettable experience. Our hotel features comfortable rooms, quality service, and modern facilities to meet all your needs. </p>
+                        <p class="slide-text">We hope you will have a pleasant and memorable stay with us.</p>
+                    </div>
+                 </div>
+            </div>
+            <div class="swiper-slide" style="background-image: url('../../images/hotel4.jpg')">
+                <div class="slide-caption text-center">
+                    <div>
+                        <p class="slide-title">Welcome to our hotel</p>
+                        <p class="slide-text">We are delighted to have you as our guest and to offer you an unforgettable experience. Our hotel features comfortable rooms, quality service, and modern facilities to meet all your needs. </p>
+                        <p class="slide-text">We hope you will have a pleasant and memorable stay with us.</p>
+                    </div>
+                 </div>
+            </div>
+
+        </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+    </div>
+    <div class="container booking-area ">
+        <form class="row">
+            <div class="col-lg mb-3 mb-lg-0">
+                <input type="date" class="form-control" placeholder="Date">
+            </div>
+            <div class="col-lg mb-3 mb-lg-0">
+                <input type="date" class="form-control" placeholder="Date">
+            </div>
+            <div class="col-lg mb-3 mb-lg-0">
+                <select class="form-select">
+                    <option selected>Adults</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                </select>
+            </div>
+            <div class="col-lg mb-3 mb-lg-0">
+                <select class="form-select">
+                    <option selected>Children</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                </select>
+            </div>
+            <div class="col-lg mb-3 mb-lg-0 mt-1">
+                <button type="submit" class="main-btn rounded-2 px-lg-3">Check Availability</button>
+            </div>
+        </form>
+    </div>
+
+</section>
