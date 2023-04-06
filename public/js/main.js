2cf0b5ba2f8c93f48120b7a4b2b51012421a9f40
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-06 06:33:14 +0000

    add slides animation

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+// Swiper Slider
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3500,
+    },
+});
