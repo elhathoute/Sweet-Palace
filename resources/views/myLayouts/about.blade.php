09d6c3e7eab8ad7afcf9ebf9c8014acccf086506
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-06 11:36:06 +0000

    add about section

---
+++ resources/views/myLayouts/about.blade.php
@@ -0,0 +1,33 @@
+<section id="about" class="about_wrapper">
+    <div class="container">
+        <div class="row d-flex align-items-center justify-content-center">
+            <div class="col-lg-6 text-center text-lg-start">
+                <h3 class="about-title mb-4">
+                    Welcome to <span class="about-sous-title">Hotel the haven</span> of your <span class="about-sous-title">Vacation</span>
+                </h3>
+                <p class="about-text">
+                    Welcome to The Sweet Palace, a luxurious hotel nestled
+                </p>
+                <p class="about-text">
+                    in the heart of the city.Since opening our doors in 2004,
+                </p>
+                <p class="about-text">
+                    we've been dedicated to providing an exceptional guest
+                </p>
+                <p class="about-text">
+                    experience, blending modern amenities with timeless
+                </p>
+                <p class="about-text mb-4">
+                    elegance and sophistication.
+                </p>
+                <a href="#" class="main-btn btn-outline-dark rounded-2 px-lg-4 py-lg-2 text-decoration-none"> Explore More</a>
+            </div>
+            <div class="col-lg-6 mb-4 mb-lg-0 ps-1g-4 text-center">
+                <img src="{{asset('/images/Hello.gif')}}" alt="hello" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
