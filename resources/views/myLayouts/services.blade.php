8e51330e14e0e36585d97eb0f02234e34706d7fd
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-08 13:23:23 +0000

    add services section

---
+++ resources/views/myLayouts/services.blade.php
@@ -0,0 +1,89 @@
+<section id="services" class="services_wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center my-5 pt-3">
+                <h6>We are Here For you</h6>
+                <h3 class="section-title">Our Awsome Services</h3>
+                <hr class="line">
+            </div>
+        </div>
+        <div class="row align-items-center service-item-wrap">
+            <div class="col-lg-7 p-lg-0">
+                <div class="tab-content" id="nav-tabContent">
+                    <div class="tab-pane fade show active" id="spa" role="tabpanel">
+                        <img src="{{asset('/images/service1.webp')}}" alt="">
+                    </div>
+                    <div class="tab-pane fade" id="restaurant" role="tabpanel">
+                        <img src="{{asset('/images/service2.webp')}}" alt="">
+                    </div>
+                    <div class="tab-pane fade" id="swimming" role="tabpanel">
+                        <img src="{{asset('/images/service3.webp')}}" alt="">
+                    </div>
+                    <div class="tab-pane fade" id="conference" role="tabpanel">
+                        <img src="{{asset('/images/service4.webp')}}" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-5 position-relative">
+                <div class="service-menu-area">
+                    <ul class="nav flex-column">
+                        <li class="">
+                            <a data-bs-toggle="tab" href="#spa" class="active service-link d-flex align-items-center">
+                                <div class="service-icon" style="height:90px; width:131px; line-height:90px">
+                                    <img src="{{asset('/images/spa.png')}}" alt="spa-icon" class="img-fluid">
+                                </div>
+                                <div>
+                                   <h5 class="service-title mt-4">Spa, Beauty & Health</h5>
+                                    <p class="service-text">
+                                        Relax and recharge your mind, body, and spirit at our hotel's tranquil spa.
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="tab" href="#restaurant" class="service-link d-flex align-items-center">
+                                <div class="service-icon" style="height:90px; width:131px; line-height:90px">
+                                    <img src="{{asset('/images/cloche.png')}}" alt="restaurant-icon" class="img-fluid">
+                                </div>
+                                <div>
+                                    <h5 class="service-title mt-4">Restaurant</h5>
+                                    <p class="service-text">
+                                        Treat your taste buds to a delectable dining experience at our hotel's restaurant.
+                                    </p>
+                                </div>
+
+                            </a>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="tab" href="#swimming" class="service-link d-flex align-items-center">
+                                <div class="service-icon" style="height:90px; width:140px; line-height:90px">
+                                    <img src="{{asset('/images/piscine.png')}}" alt="piscine-icon" class="img-fluid">
+                                </div>
+                                <div>
+                                    <h5 class="service-title mt-4">Swimming Pool</h5>
+                                    <p class="service-text">
+                                        Cool off and unwind in our hotel's inviting swimming pool, the perfect place to relax and recharge.
+                                    </p>
+                                </div>
+                            </a>
+                        </li>
+                        <li>
+                            <a data-bs-toggle="tab" href="#conference" class="service-link  d-flex align-items-center">
+                                <div class="service-icon" style="height:90px; width:105px; line-height:90px">
+                                    <img src="{{asset('/images/conference.png')}}" alt="conference-icon" class="img-fluid">
+                                </div>
+                                <div>
+                                    <h5 class="service-title mt-4">Conference Hall</h5>
+                                    <p class="service-text">
+                                        Impress your guests with a stunning event in our hotel's elegant conference hall
+                                    </p>
+                                </div>
+
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
