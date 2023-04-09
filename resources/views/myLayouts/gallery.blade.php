080f8ca5055874eda6c54dec3a8464367cc75033
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-09 13:19:35 +0000

    create new layouts

---
+++ resources/views/myLayouts/gallery.blade.php
@@ -0,0 +1,50 @@
+<section id="gallery" class="gallery_wrapper">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 text-center my-5">
+                <h6>Best Pictures Of Our Hotel</h6>
+                <h3 class="section-title">Our Gallery</h3>
+                <hr class="line">
+            </div>
+        </div>
+        <div class="row g-0">
+            <div class="col-lg-3 col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery1.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery2.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery3.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery4.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+            <div class="col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery5.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+            <div class="col-md-6 gallery-item">
+                <img src="{{asset('/images/gallery6.jpg')}}" alt="" class="img-fluid w-100">
+                <div class="gallery-item-content">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
