798736ea5cc7ff6d64eae775560e4e5ca1eae934
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-11 01:25:51 +0000

    create view

--- resources/views/myLayouts/roomType/index.blade.php
+++ resources/views/myLayouts/roomType/index.blade.php
@@ -1,1 +1,37 @@
-all data
+
+@extends('myLayouts.dashboard')
+@section('content')
+<table id="example" class="display table table-responsive-lg" style="width:100%">
+    <thead class="table-dark">
+        <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011-04-25</td>
+            <td>$320,800</td>
+        </tr>
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </tfoot>
+</table>
+    
+@endsection
