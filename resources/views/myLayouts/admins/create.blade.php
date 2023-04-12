4825e2e3508ccc4fb25e2fad64a4c405a9dca760
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-12 12:16:43 +0000

    add create admin view

---
+++ resources/views/myLayouts/admins/create.blade.php
@@ -0,0 +1,82 @@
+
+@extends('myLayouts.dashboard')
+@section('content')
+<div class="container-fluid">
+    @if (Session::has('success'))
+        <p class="text-success">{{session('success')}}</p>
+    @endif
+    <div class="card shadow my-5">
+        <div class="card-header py-3 d-flex align-items-center justify-content-between">
+            <h6 class="m-0 fw-bold">Add Admin</h6>
+            <a href="{{url('myLayouts/admins')}}" class="text-decoration-none btn btn-success">View All</a>
+        </div>
+
+        <div class="card-body">
+            <form method="post" action="{{url('myLayouts/admins')}}" enctype="multipart/form-data">
+                @csrf
+                <div class="mb-3">
+                    <label for="picture" class="form-label">Picture</label>
+                    <input type="file" class="form-control" id="picture" name="picture">
+                    @error('picture')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="firstname" class="form-label">Firstname</label>
+                    <input type="text" class="form-control" id="firstname" name="firstname">
+                    @error('firstname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="form-label">Lastname</label>
+                    <input type="text" class="form-control" id="lastname" name="lastname">
+                    @error('lastname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="email" class="form-control" id="email" name="email">
+                    @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                    @error('password')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                    @error('password_confirmation')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="mobile" class="form-label">Mobile</label>
+                    <input type="text" class="form-control" id="mobile" name="mobile">
+                    @error('mobile')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="address" name="address">
+                    @error('address')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary my-4">Submit</button>
+            </form>
+
+        </div>
+    </div>
+
+</div>
+
+@endsection
