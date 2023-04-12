4b3ebc0de66e30234b2dfd8e7c02296f00b2c56e
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-12 12:16:56 +0000

    add edit admin view

---
+++ resources/views/myLayouts/admins/edit.blade.php
@@ -0,0 +1,84 @@
+@extends('myLayouts.dashboard')
+@section('content')
+
+<div class="container-fluid">
+    @if (Session::has('success'))
+        <p class="alert alert-success mt-1 mb-1">{{session('success')}}</p>
+    @endif
+
+    <div class="card shadow my-5">
+        <div class="card-header py-3 d-flex align-items-center justify-content-between">
+            <h6 class="m-0 fw-bold">Update Admin</h6>
+            <a href="{{url('myLayouts/admins')}}" class="text-decoration-none btn btn-success">View All</a>
+        </div>
+
+        <div class="card-body">
+            <form method="post" action="{{url ('myLayouts/admins/'. $admin->id)}}" enctype="multipart/form-data">
+                @csrf
+                @method('put')
+                <input type="hidden" name="id" id="id" value="{{$admin->id}}">
+                <div class="mb-3">
+                    <label for="picture" class="form-label">Picture</label>
+                    <input type="file" class="form-control" id="picture" name="picture" value="{{$admin->picture}}">
+                    @error('picture')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="firstname" class="form-label">Firstname</label>
+                    <input type="text" class="form-control" id="firstname" name="firstname" value="{{$admin->firstname}}">
+                    @error('firstname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="lastname" class="form-label">Lastname</label>
+                    <input type="text" class="form-control" id="lastname" name="lastname"  value="{{$admin->lastname}}">
+                    @error('lastname')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email Address</label>
+                    <input type="email" class="form-control" id="email" name="email" value="{{$admin->email}}">
+                    @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">New Password</label>
+                    <input type="password" class="form-control" id="password" name="password">
+                    @error('password')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="password_confirmation" class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                    @error('password_confirmation')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="mobile" class="form-label">Mobile</label>
+                    <input type="text" class="form-control" id="mobile" name="mobile"  value="{{$admin->mobile}}">
+                    @error('mobile')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="address" class="form-label">Address</label>
+                    <input type="text" class="form-control" id="address" name="address" value="{{$admin->address}}">
+                    @error('address')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <button type="submit" class="btn btn-primary my-4">Submit</button>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+@endsection
