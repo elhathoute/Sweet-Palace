05b49cb91ba0483f07847c3713c7a3ec8a4ad559
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-14 09:34:25 +0000

    add principal page

---
+++ resources/views/myLayouts/roomPage.blade.php
@@ -0,0 +1,307 @@
+<section class="container" style="max-width: 1600px !important;">
+    <div class="row gy-4 justify-content-center">
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/HS69JBASW4Q8.jpg" alt="image" class="snipcss0-0-0-1">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="TV">
+                            <i class="la la-tv"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="#">
+                            Executive Suite
+                        </a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                100.00
+                                USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 2
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 1
+                                </span>
+                                <a href="#" class="btn btn-sm btn--base"><i class="la la-desktop me-2"></i>DETAILS</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/Q9G88MCC97X8.jpg" alt="image">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Pool">
+                            <i class="la la-swimmer"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>                                        
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="#">President Suite</a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                250.00 USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 2
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 0
+                                </span>
+                                <a href="#" class="btn btn-sm btn--base">
+                                    <i class="la la-desktop me-2"></i>
+                                    DETAILS
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/62985ef5c90521654152949.jpg" alt="image">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Pool">
+                            <i class="la la-swimmer"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="#">Murphy</a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text-base mb-3">
+                                230.00 USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 2
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 1
+                                </span>
+                                <a href="#" class="btn btn-sm btn-base"><i class="la la-desktop me-2"></i>DETAILS</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/5DQ2Q5WR82S6.jpg" alt="image">
+                        <ul class="room-card__utilities">
+                            <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                                <i class="la la-wifi"></i>                                        
+                            </li>
+                            <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                                <i class="la la-business-time"></i>                                        
+                            </li>
+                            <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="TV">
+                                <i class="la la-tv"></i>                                        
+                            </li>
+                        </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="">Mini Suite</a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                12.00 USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 1
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 0
+                                </span>
+                                <a href="#" class="btn btn-sm btn--base">
+                                    <i class="la la-desktop me-2"></i>
+                                        DETAILS
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/RCUDWGO3H29K.jpg" alt="image">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"> </i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="">
+                            Walker Cantrell
+                        </a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                60.00 USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 1
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 0
+                                </span>
+                                <a href="#" class="btn btn-sm btn--base"><i class="la la-desktop me-2"></i>DETAILS</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/1QXG51HUNCUS.jpg" alt="image">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Pool">
+                            <i class="la la-swimmer"></i>                                        
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>                                        
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="#">Luxury Suite</a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                    220.00 USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                    Adult &nbsp; 2
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 0
+                                </span>
+                                <a href="#" class="btn btn-sm btn--base"><i class="la la-desktop me-2"></i>DETAILS</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-4 col-md-6 col-xs-10">
+            <div class="room-card">
+                <div class="room-card__thumb">
+                    <img src="https://script.viserlab.com/viserhotel/assets/images/roomType/HGZ28VU2XTRB.jpg" alt="image">
+                    <ul class="room-card__utilities">
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Unlimited Wifi">
+                            <i class="la la-wifi"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="AC">
+                            <i class="la la-business-time"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Pool">
+                            <i class="la la-swimmer"></i>
+                        </li>
+                        <li data-bs-toggle="tooltip" data-bs-placement="right" title="" data-bs-original-title="Washing Machine">
+                            <i class="la la-tshirt"></i>
+                        </li>
+                    </ul>
+                </div>
+                <div class="room-card__content">
+                    <h3 class="title mb-2">
+                        <a href="#">
+                            Accessible Room
+                        </a>
+                    </h3>
+                    <div class="room-card__bottom justify-content-between align-items-center mt-2 gap-3">
+                        <div>
+                            <h6 class="price text--base mb-3">
+                                350.00
+                                USD / Night                                        
+                            </h6>
+                            <div class="room-capacity text--base d-flex align-items-center flex-wrap gap-3">
+                                <span class="custom--badge">
+                                Adult &nbsp; 2
+                                </span>
+                                <span class="custom--badge">
+                                    Child &nbsp; 2
+                                </span>
+                                <a href="" class="btn btn-sm btn--base"><i class="la la-desktop me-2"></i>DETAILS</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+  
