e903b458503e78e354924f1a4a0520e04f1557c8
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-16 14:15:50 +0000

    staff migration

---
+++ database/migrations/2023_04_16_113605_create_staff_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('staff', function (Blueprint $table) {
+            $table->id();
+            $table->string('photo');
+            $table->string('full_name');
+            $table->integer('departement_id');
+            $table->text('bio');
+            $table->string('salary_type');
+            $table->string('salary_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('staff');
+    }
+};
