196d225270862a46e0c46b2709e5f6b02d1fe467
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-17 15:11:29 +0000

    create model & Relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,8 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'mobile',
+        'address',
     ];
 
     /**
@@ -41,4 +43,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
 }
