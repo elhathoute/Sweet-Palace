b89d0c4c8341cb354f3b63ee6d904a2c0277cc1f
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-17 15:10:50 +0000

    create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('mobile');
+            $table->string('address');
+            $table->integer('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
