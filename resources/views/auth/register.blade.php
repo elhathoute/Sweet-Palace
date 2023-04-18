1d354f446ad6c39dca54aa04e050c8d46c78eb3f
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-18 03:50:45 +0000

    update & add style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,8 +9,8 @@
             <div class="card border-0">
                 <div class="card-body">
                     <div class="card-title">
-                        <h3 class="text-center">Sign Up Account</h3>
-                        <p class="text-center">Please provide the following information to sign up for our hotel</p>
+                        <h3 class="text-center capital-title">Create your Account</h3>
+                        <p class="text-center sous-title mt-3">Please provide the following informations to sign up for our hotel</p>
                     </div>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -18,9 +18,9 @@
                         <div class="row mb-3">
                             <label for="name" class="col-md-12 col-form-label ">{{ __('Name') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12 user-input">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
-
+                                <i class='bx bxs-user'></i>
                                 @error('name')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -32,9 +32,9 @@
                         <div class="row mb-3">
                             <label for="email" class="col-md-12 col-form-label">{{ __('Email Address') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12 user-input">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
+                                <i class='bx bxs-envelope' ></i>
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -46,9 +46,9 @@
                         <div class="row mb-3">
                             <label for="mobile" class="col-md-12 col-form-label">{{ __('Mobile Number') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12 user-input">
                                 <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autocomplete="mobile">
-
+                                <i class='bx bxs-phone' ></i>
                                 @error('mobile')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -59,9 +59,9 @@
                         <div class="row mb-3">
                             <label for="address" class="col-md-12 col-form-label">{{ __('Address') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12 user-input">
                                 <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ old('address') }}" required autocomplete="address">
-
+                                <i class='bx bxs-map' ></i>
                                 @error('address')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -73,9 +73,9 @@
                         <div class="row mb-3">
                             <label for="password" class="col-md-12 col-form-label">{{ __('Password') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12 user-input">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                                <i class='bx bx-key' ></i>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -87,8 +87,9 @@
                         <div class="row mb-3">
                             <label for="password-confirm" class="col-md-12 col-form-label">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-12">
+                            <div class="col-md-12  user-input">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <i class='bx bx-key' ></i>
                             </div>
                         </div>
 
