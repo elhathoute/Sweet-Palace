73496a613a4972cf9c741a35bff73458aa56d36a
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-25 14:13:16 +0100

    add migration

---
+++ database/migrations/2023_04_25_091958_create_amenity_room_type_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('amenity_room_type', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('room_type_id');
+            $table->unsignedBigInteger('amenity_id');
+
+            $table->foreign('room_type_id')
+                ->references('id')
+                ->on('room_types')
+                ->onDelete('cascade');
+
+            $table->foreign('amenity_id')
+                ->references('id')
+                ->on('amenities')
+                ->onDelete('cascade');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('amenity_room_type');
+    }
+};
