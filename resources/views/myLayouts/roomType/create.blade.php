5e171290d421c3c68857ba051b89e889a2cab3dd
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-25 17:19:20 +0100

    update

--- resources/views/myLayouts/roomType/create.blade.php
+++ resources/views/myLayouts/roomType/create.blade.php
@@ -43,13 +43,47 @@
                     @enderror
                 </div>
                 <div class="mb-3">
+                    <label for="amenities" class="form-label">Amenities</label>
+                    <select class="form-select" id="amenities" name="amenities[]" multiple>
+                        @foreach ($amenities as $at)
+                            <option value="{{$at->id}}">{{$at->name}}</option>
+                        @endforeach
+                    </select>
+                    @error('amenities')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="beds" class="form-label">Beds</label>
+                    <select class="form-select" id="beds" name="beds[]" multiple>
+                        @foreach ($beds as $bed)
+                            <option value="{{$bed->id}}">{{$bed->name}}</option>
+                        @endforeach
+                    </select>
+                    @error('beds')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+                <div class="mb-3">
+                    <label for="complements" class="form-label">Complements</label>
+                    <select class="form-select" id="complements" name="complements[]" multiple>
+                        @foreach ($complements as $complement)
+                            <option value="{{$complement->id}}">{{$complement->name}}</option>
+                        @endforeach
+                    </select>
+                    @error('complements')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                    @enderror
+                </div>
+
+                <div class="mb-3">
                     <label for="price" class="form-label">Price</label>
                     <input type="number" class="form-control" id="price" name="price">
                     @error('price')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                     @enderror
                 </div>
-                
+
                 <div class="mb-3">
                     <label for="image" class="form-label">Cover</label>
                     <input type="file" class="form-control" id="image" name="image_path">
