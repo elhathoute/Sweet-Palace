06060f069b9d71f92fb7fae0b3172b72af3a61cd
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-04-25 14:12:33 +0100

    add migration

--- database/migrations/2023_04_22_141404_create_amenities_table.php
+++ database/migrations/2023_04_22_141404_create_amenities_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('amenities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('room_type_id');
             $table->timestamps();
         });
     }
